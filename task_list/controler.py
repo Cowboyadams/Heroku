5b01d4643bed2cfac5b159e9eb3583a3937250ee
Dallin Adams <d.rob.adams@gmail.com>
2022-08-31 05:05:46 -0600

    back to working commit

--- task_list/controler.py
+++ task_list/controler.py
@@ -1,17 +1,29 @@
 from flask import (
-    Blueprint, flash, redirect, render_template, request, url_for
+    Flask, Blueprint, flash, redirect, render_template, request, url_for
 )
 
 from task_list import db
 from task_list.models import Songs, Weather
 
+from model import db
+
+app = Flask(__name__)
 bp = Blueprint('task_list', __name__)
 app = Flask(__name__)
 
 
+<<<<<<< HEAD
 @bp.route('/', methods=('GET', 'POST'))
 def index():
     
    return render_template('task_list/index.html')
 
 
+=======
+@app.route("/")
+def welcome():
+    return render_template(
+        "index.html",
+        test="wow this is crazy"
+        )
+>>>>>>> parent of d41ca91 (changes to Controler file)
