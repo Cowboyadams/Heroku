dbd90d357c228110099524d130727166417672e5
Dallin Adams <d.rob.adams@gmail.com>
2022-08-31 04:20:53 -0600

    changing routes

--- task_list/controler.py
+++ task_list/controler.py
@@ -1,14 +1,18 @@
 from flask import (
-    Blueprint, flash, redirect, render_template, request, url_for
+    Flask, Blueprint, flash, redirect, render_template, request, url_for
 )
 
 from task_list import db
 from task_list.models import Songs, Weather
 
+from model import db
+
+app = Flask(__name__)
 bp = Blueprint('task_list', __name__)
 
-@bp.route('/')
-def index():
-   
-    
-    return render_template('task_list/index.html')
+@app.route("/")
+def welcome():
+    return render_template(
+        "index.html",
+        test="wow this is crazy"
+        )
