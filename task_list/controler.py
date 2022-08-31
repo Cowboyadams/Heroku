75be07dbaa6e66b24b7eb118e3cce7f7e7160f6a
Dallin Adams <d.rob.adams@gmail.com>
2022-08-31 05:32:51 -0600

    adjusting controller file

--- task_list/controler.py
+++ task_list/controler.py
@@ -9,13 +9,10 @@ bp = Blueprint('task_list', __name__)
 
 @bp.route('/', methods=('GET', 'POST'))
 def index():
-   
-    return render_template('task_list/index.html')
+    if request.method == 'GET':
+        songs = "zoowee"
 
-@bp.route('/<int:id>/delete', methods=('POST',))
-def delete(id):
-    task = Task.query.get(id)
-    if task != None:
-        db.session.delete(task)
-        db.session.commit()
-    return redirect(url_for('task_list.index'))
+        return render_template('task_list/index.html')
+    else:
+        return render_template('task_list/index.html')
+
