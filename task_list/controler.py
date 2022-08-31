c42df4a03ee5f32d022f0976f92445c79bc083ac
Dallin Adams <d.rob.adams@gmail.com>
2022-08-31 04:02:17 -0600

    fixed procfile parse bug

--- task_list/controler.py
+++ task_list/controler.py
@@ -10,6 +10,5 @@ bp = Blueprint('task_list', __name__)
 @bp.route('/')
 def index():
    
-    songs = Songs.query.all()
-    weather = Weather.query.all()
+    
     return render_template('task_list/index.html')
