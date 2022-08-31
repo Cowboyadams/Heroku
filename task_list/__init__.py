4fc4bac907cbac0509f8b48eb1a77455b67aa902
Dallin Adams <d.rob.adams@gmail.com>
2022-08-31 05:19:26 -0600

    update v1

--- task_list/__init__.py
+++ task_list/__init__.py
@@ -11,8 +11,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_mapping(
         SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev_key',
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-            'sqlite:///' + os.path.join(app.instance_path, 'task_list.sqlite'),
+        SQLALCHEMY_DATABASE_URI = "postgres://lbyxwmuqvbndrv:948908345954624da81b9a539c830d5122aff868c7f927efc3ce5e5a2bf31350@ec2-44-205-63-142.compute-1.amazonaws.com:5432/df6ube44s5bk3",
         SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
 
--- task_list/controler.py
+++ task_list/controler.py
@@ -1,18 +1,29 @@
 from flask import (
-    Flask, Blueprint, flash, redirect, render_template, request, url_for
+    Blueprint, flash, redirect, render_template, request, url_for
 )
 
 from task_list import db
-from task_list.models import Songs, Weather
+from task_list.models import Task
 
-from model import db
-
-app = Flask(__name__)
 bp = Blueprint('task_list', __name__)
 
-@app.route("/")
-def welcome():
-    return render_template(
-        "index.html",
-        test="wow this is crazy"
-        )
+@bp.route('/', methods=('GET', 'POST'))
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        if not name:
+            flash('Task name is required.')
+        else:
+            db.session.add(Task(name=name))
+            db.session.commit()
+
+    tasks = Task.query.all()
+    return render_template('task_list/index.html', tasks=tasks)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    task = Task.query.get(id)
+    if task != None:
+        db.session.delete(task)
+        db.session.commit()
+    return redirect(url_for('task_list.index'))
