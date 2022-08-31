476249db9417888e9c28e14848de88faffc5454e
Dallin Adams <d.rob.adams@gmail.com>
2022-08-31 04:59:59 -0600

    changing route from bp to app

--- task_list/controler.py
+++ task_list/controler.py
@@ -6,19 +6,26 @@ from task_list import db
 from task_list.models import Songs, Weather
 
 bp = Blueprint('task_list', __name__)
+app = Flask(__name__)
 
-@bp.route('/', methods=('GET', 'POST'))
-def index():
-    if request.method == 'POST':
-        name = request.form['name']
-        if not name:
-            flash('Task name is required.')
-        else:
-            db.session.add(Songs(name=name))
-            db.session.commit()
-
-    tasks = Songs.query.all()
-    return render_template('task_list/index.html', tasks=tasks)
+@app.route("/")
+def welcome():
+    return render_template(
+        "index.html",
+        test="wow this is crazy"
+        )
+#@bp.route('/', methods=('GET', 'POST'))
+#def index():
+#    if request.method == 'POST':
+#        name = request.form['name']
+#        if not name:
+#            flash('Task name is required.')
+#        else:
+#            db.session.add(Songs(name=name))
+#            db.session.commit()
+#
+#    tasks = Songs.query.all()
+#    return render_template('task_list/index.html', tasks=tasks)
 
 @bp.route('/<int:id>/delete', methods=('POST',))
 def delete(id):
