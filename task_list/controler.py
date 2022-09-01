cb621e0c5cb5d126bbcd2f67ef6ac0abaa7d02aa
Dallin Adams <d.rob.adams@gmail.com>
2022-09-01 16:42:17 -0600

    added location finder to app

--- task_list/controler.py
+++ task_list/controler.py
@@ -17,18 +17,22 @@ def index():
         return render_template('index.html')
     
     if request.method == 'POST':
-        #The users input (zipcode)
+        # The users input (zipcode)
         user_zipcode = request.form['zipcode']
         print("\n the users zipcode is: \n" + user_zipcode + "\n")
         
-        #Getting weather Data from Weather API
+        # Getting weather Data from Weather API
         weather_api = ('http://api.weatherapi.com/v1/forecast.json?key=6e399ba00ff24135ae584519223108&q=' + user_zipcode + '&days=1&aqi=no&alerts=no')
         weather_req = requests.get(weather_api)
         weather_res = weather_req.json()
         weather_res_text = weather_req.text
         print("the weather api results are: \n" + weather_res_text + "\n")
 
+        # Declaring Variables
+        user_local = weather_res["location"]["name"]
+
         return render_template(
-            'index.html'
+            'index.html',
+            user_local=user_local
             )
 
